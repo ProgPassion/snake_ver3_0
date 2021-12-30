2053c673542058f5f9808bca251c5b1bf665e592
ProgPassion <31067252+ProgPassion@users.noreply.github.com>
2021-12-30 23:21:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,219 @@
-const fieldElement = document.querySelector(".field");
-const fieldWidth   = 500;
-const fieldHeight  = 800;
-const cellSize     = 25;
 
-const bodyCoordinates = [{cellX: 4, cellY: 7, cellBackgroundClass: "snakeHead"}];
-const topEdge    = 0;
-const bottomEdge = fieldHeight / cellSize - 1;
-const leftEdge   = 0;
-const rightEdge  = fieldWidth / cellSize - 1;
+class Cell {
+	constructor(cellSize) {
+		this.cellSize = cellSize;
+		this.cellSizeCssStyle = `.cellsize {width: ${cellSize}px; height: ${cellSize}px;}`;
+	}
+}
+
+class FieldGame extends Cell {
+	constructor(fieldWidth, fieldHeight, cellSize) {
+		super(cellSize);
+		
+		this.fieldElement = document.querySelector(".field");
+		this.fieldWidth   = fieldWidth;
+		this.fieldHeight  = fieldHeight;
+		this.topEdge      = 0;
+		this.bottomEdge   = fieldHeight / cellSize - 1;
+		this.leftEdge     = 0;
+		this.rightEdge    = fieldWidth / cellSize - 1;
+	}
+
+	iterateAllFieldCells(actionInsideIteration) {
+		for(let i = this.fieldHeight, cellY = 0; i > 0; i -= this.cellSize, cellY++) {
+			for(let j = this.fieldWidth, cellX = 0; j > 0; j -= this.cellSize, cellX++) {
+				actionInsideIteration(cellX, cellY);
+			}
+		}
+	}
+}
+
+class Snake {
+	constructor() {
+		this.bodyCoordinates = [{cellX: 4, cellY: 7, cellBackgroundClass: "snakeHead"}];
+		this.direction = "right";
+		this.movementCellX = 1;
+		this.movementCellY = 0;
+	}
 
 
-let direction = "right";
-let movementCellX = 1;
-let movementCellY = 0;
+	updateSnakeDirection() {
 
-let gameOver = false;
+		switch(this.direction) {
+			case "right" :
+				this.movementCellX = 1;
+				this.movementCellY = 0;
+				break;
+			case "left" :
+				this.movementCellX = -1;
+				this.movementCellY = 0;
+				break;
+			case "up" :
+				this.movementCellX = 0;
+				this.movementCellY = -1;
+				break;
+			case "down" :
+				this.movementCellX = 0;
+				this.movementCellY = 1;
+				break;
+		}
+	}
 
-let fruitPos = generateFruitRandomLocation();
+	updateSnakeHeadPos(field) {
+
+		if(this.direction == "right") {
+			if(this.bodyCoordinates[0].cellX === field.rightEdge)
+				this.bodyCoordinates[0].cellX = field.leftEdge; 
+			else 
+				this.bodyCoordinates[0].cellX += this.movementCellX;	
+		}
+
+		else if(this.direction == "left") {
+			if(this.bodyCoordinates[0].cellX === field.leftEdge)
+				this.bodyCoordinates[0].cellX = field.rightEdge; 
+			else 
+				this.bodyCoordinates[0].cellX += this.movementCellX;	
+		}
+
+		else if(this.direction == "up") {
+			if(this.bodyCoordinates[0].cellY === field.topEdge) 
+				this.bodyCoordinates[0].cellY = field.bottomEdge;
+			else 
+				this.bodyCoordinates[0].cellY += this.movementCellY;	
+		}
+
+		else if(this.direction == "down") {
+			if(this.bodyCoordinates[0].cellY === field.bottomEdge) 
+				this.bodyCoordinates[0].cellY = field.topEdge;
+			else 
+				this.bodyCoordinates[0].cellY += this.movementCellY;	
+		}
+		
+	}
+
+
+	updateSnakeBodyPos() {
+
+		for(let index = this.bodyCoordinates.length - 1; index >= 1; index--) {
+			if(this.bodyCoordinates[index].cellBackgroundClass === "snakeHead")
+				this.bodyCoordinates[index].cellBackgroundClass = "snakeBody";
+
+			this.bodyCoordinates[index].cellX = this.bodyCoordinates[index-1].cellX;
+			this.bodyCoordinates[index].cellY = this.bodyCoordinates[index-1].cellY;	
+		}
+		
+	}
+
+	checkIfSnakeAteFruit(fruitPosX, fruitPosY) {
+
+		if(this.bodyCoordinates[0].cellX === fruitPosX && this.bodyCoordinates[0].cellY === fruitPosY) {
+			this.bodyCoordinates.push({cellX: fruitPosX, cellY: fruitPosY, cellBackgroundClass: "snakeHead"});
+			return true;	
+		}
+	}
+
+	controllForGameOverEvent() {
+
+		for(let index = this.bodyCoordinates.length - 1; index >= 1; index--) {
+			if(this.bodyCoordinates[0].cellX === this.bodyCoordinates[index].cellX && 
+				this.bodyCoordinates[0].cellY === this.bodyCoordinates[index].cellY)
+				return true;
+		}
+	}
+
+}
+
+class Fruit {
+	constructor() {
+		this.fruitPosX;
+		this.fruitPosY;
+	}
+	generateFruitRandomLocation(snakeBodyCoordinates, rightEdge, bottomEdge) {
+
+		function checkIfLocationIsFree(fruitPosX, fruitPosY) {
+
+			for(let i = 0; i < snakeBodyCoordinates.length; i++) {
+				if(snakeBodyCoordinates[i].cellX == fruitPosX && snakeBodyCoordinates[i].cellY == fruitPosY)
+					return false;
+			}
+			return true;
+		}
+
+		do {
+			this.fruitPosX = Math.floor(Math.random() * rightEdge);
+			this.fruitPosY = Math.floor(Math.random() * bottomEdge);
+		} while(!checkIfLocationIsFree(this.fruitPosX, this.fruitPosY));
+
+		return {
+			posX: this.fruitPosX,
+			posY: this.fruitPosY,
+			cellBackgroundClass: "fruit",
+		};
+	}
+}
+
+
+
+
+
+const field = new FieldGame(500,800,25);
+field.iterateAllFieldCells(function(cellX, cellY) {
+	field.fieldElement.innerHTML += `<span id="cell_${cellX}_${cellY}" class="cell cellsize"></span>`;
+});
 
 ////////////////////////////////////////////////////////////////////////////////
 
 const sheet = document.createElement('style');
-sheet.innerHTML = `.cellsize {width: ${cellSize}px; height: ${cellSize}px;}`;
+sheet.innerHTML = field.cellSizeCssStyle;
 document.head.appendChild(sheet);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-function iterateAllFieldCells(actionInsideIteration) {
+const snake = new Snake();
+const fruit = new Fruit();
 
-	for (let i = fieldHeight, cellY = 0; i > 0; i -= cellSize, cellY++) {
-		for(let j = fieldWidth, cellX = 0; j > 0; j -= cellSize, cellX++) {
-			actionInsideIteration(cellX, cellY);
-		}
+let fruitPos = fruit.generateFruitRandomLocation(snake.bodyCoordinates, field.rightEdge, field.bottomEdge);
+
+function keyDownHandle(event) {
+
+	if(event.keyCode == 87 && snake.direction != "down") {
+		snake.direction = "up";
+		document.removeEventListener('keydown', keyDownHandle);
+		snake.updateSnakeDirection();
+	}
+	else if(event.keyCode == 83 && snake.direction != "up") {
+		snake.direction = "down";
+		document.removeEventListener('keydown', keyDownHandle);
+		snake.updateSnakeDirection();
+	}
+	else if(event.keyCode == 65 && snake.direction != "right") {
+		snake.direction = "left";
+		document.removeEventListener('keydown', keyDownHandle);
+		snake.updateSnakeDirection();
+	}
+	else if(event.keyCode == 68 && snake.direction != "left") {
+		snake.direction = "right";
+		document.removeEventListener('keydown', keyDownHandle);
+		snake.updateSnakeDirection();
 	}
 }
 
-function iterateSnakeBody(actionInsideIteration) {
+let playGameCircle = setInterval(function() {
+	
 
-	for(let i = bodyCoordinates.length - 1; i >=1; i--) {
-		actionInsideIteration(i);
-	}
-}
+	document.addEventListener('keydown', keyDownHandle, {once: true});
+	snake.updateSnakeBodyPos();
+	snake.updateSnakeHeadPos(field);
 
-
-
-function buildField() {
-	iterateAllFieldCells(function(cellX, cellY) {
-		fieldElement.innerHTML += `<span id="cell_${cellX}_${cellY}" class="cell cellsize"></span>`;
-	}); 	
-}
-
-function generateFruitRandomLocation() {
-
-	let fruitPosX;
-	let fruitPosY;
-
-	function checkIfLocationIsFree(fruitPosX, fruitPosY) {
-
-		for(let i = 0; i < bodyCoordinates.length; i++) {
-			if(bodyCoordinates[i].cellX == fruitPosX && bodyCoordinates[i].cellY == fruitPosY)
-				return false;
-		}
-		return true;
+	if(snake.controllForGameOverEvent()){
+		clearInterval(playGameCircle);
+		alert("Game Over!\n\nReload the page to play again...");			
 	}
 
-	do {
-
-		fruitPosX = Math.floor(Math.random() * rightEdge);
-		fruitPosY = Math.floor(Math.random() * bottomEdge);
-		
-	} while(!checkIfLocationIsFree(fruitPosX, fruitPosY));
-
-	return {
-		posX: fruitPosX,
-		posY: fruitPosY,
-		cellBackgroundClass: "fruit"
-	};
-}
-
-function printGameElements() {
-
-	iterateAllFieldCells(function(cellX, cellY) {
+	field.iterateAllFieldCells(function(cellX, cellY) {
 		document.getElementById(`cell_${cellX}_${cellY}`).classList = "cell cellsize";
 
-		bodyCoordinates.forEach(function(currentCell) {
+		snake.bodyCoordinates.forEach(function(currentCell) {
 			if(currentCell.cellX == cellX && currentCell.cellY == cellY) {
 				document.getElementById(`cell_${cellX}_${cellY}`).classList.add(currentCell.cellBackgroundClass);
 			}
@@ -91,123 +221,10 @@ function printGameElements() {
 
 		if(fruitPos.posX == cellX && fruitPos.posY == cellY)
 			document.getElementById(`cell_${cellX}_${cellY}`).classList.add(fruitPos.cellBackgroundClass);
+
+		if(snake.checkIfSnakeAteFruit(fruitPos.posX, fruitPos.posY))
+			fruitPos = fruit.generateFruitRandomLocation(snake.bodyCoordinates, field.rightEdge, field.bottomEdge);
+	
 	});	
-}
 
-function updateSnakeDirection() {
-
-	switch(direction) {
-		case "right" :
-			movementCellX = 1;
-			movementCellY = 0;
-			break;
-		case "left" :
-			movementCellX = -1;
-			movementCellY = 0;
-			break;
-		case "up" :
-			movementCellX = 0;
-			movementCellY = -1;
-			break;
-		case "down" :
-			movementCellX = 0;
-			movementCellY = 1;
-			break;
-	}
-}
-
-function updateSnakeHeadPos() {
-
-	if(direction == "right") {
-		if(bodyCoordinates[0].cellX === rightEdge)
-			bodyCoordinates[0].cellX = leftEdge; 
-		else 
-			bodyCoordinates[0].cellX += movementCellX;	
-	}
-
-	else if(direction == "left") {
-		if(bodyCoordinates[0].cellX === leftEdge)
-			bodyCoordinates[0].cellX = rightEdge; 
-		else 
-			bodyCoordinates[0].cellX += movementCellX;	
-	}
-
-	else if(direction == "up") {
-		if(bodyCoordinates[0].cellY === topEdge) 
-			bodyCoordinates[0].cellY = bottomEdge;
-		else 
-			bodyCoordinates[0].cellY += movementCellY;	
-	}
-
-	else if(direction == "down") {
-		if(bodyCoordinates[0].cellY === bottomEdge) 
-			bodyCoordinates[0].cellY = topEdge;
-		else 
-			bodyCoordinates[0].cellY += movementCellY;	
-	}
-	
-}
-
-function updateSnakeBodyPos(index) {
-
-	if(bodyCoordinates[index].cellBackgroundClass === "snakeHead")
-		bodyCoordinates[index].cellBackgroundClass = "snakeBody";
-
-	bodyCoordinates[index].cellX = bodyCoordinates[index-1].cellX;
-	bodyCoordinates[index].cellY = bodyCoordinates[index-1].cellY;
-}
-
-function checkIfSnakeAteFruit() {
-	
-	if(bodyCoordinates[0].cellX === fruitPos.posX && bodyCoordinates[0].cellY === fruitPos.posY) {
-		bodyCoordinates.push({cellX: fruitPos.posX, cellY: fruitPos.posY, cellBackgroundClass: "snakeHead"});
-		fruitPos = generateFruitRandomLocation();	
-	}
-}
-
-function controllForGameOverEvent(index) {
-
-	if(bodyCoordinates[0].cellX === bodyCoordinates[index].cellX && bodyCoordinates[0].cellY === bodyCoordinates[index].cellY)
-		gameOver = true;
-}
-
-function keyDownHandle(event) {
-
-	if(event.keyCode == 87 && direction != "down") {
-		direction = "up";
-		document.removeEventListener('keydown', keyDownHandle);
-		updateSnakeDirection();
-	}
-	else if(event.keyCode == 83 && direction != "up") {
-		direction = "down";
-		document.removeEventListener('keydown', keyDownHandle);
-		updateSnakeDirection();
-	}
-	else if(event.keyCode == 65 && direction != "right") {
-		direction = "left";
-		document.removeEventListener('keydown', keyDownHandle);
-		updateSnakeDirection();
-	}
-	else if(event.keyCode == 68 && direction != "left") {
-		direction = "right";
-		document.removeEventListener('keydown', keyDownHandle);
-		updateSnakeDirection();
-	}
-}
-
-buildField();
-let playGameCircle = setInterval(function() {
-
-	if(!gameOver) {
-		document.addEventListener('keydown', keyDownHandle, {once: true});
-		iterateSnakeBody(updateSnakeBodyPos);
-		updateSnakeHeadPos();
-		iterateSnakeBody(controllForGameOverEvent);
-		checkIfSnakeAteFruit();
-		printGameElements();	
-	}
-	else {
-		clearInterval(playGameCircle);
-		alert("Game Over!\n\nReload the page to play again...");
-	}
 }, 150);
